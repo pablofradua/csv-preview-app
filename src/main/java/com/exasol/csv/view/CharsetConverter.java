a5f98fcd4ae7266840b83606dc1948e869f510a1
Pablo Fradua <pablo.fradua@gmail.com>
2021-08-02 00:14:31 +0200

    Custom JSF Converter for the Charset class

---
+++ src/main/java/com/exasol/csv/view/CharsetConverter.java
@@ -0,0 +1,23 @@
+package com.exasol.csv.view;
+
+import java.nio.charset.Charset;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter("com.exasol.csv.view.CharsetConverter")
+public class CharsetConverter implements Converter<Charset> {
+
+	@Override
+	public Charset getAsObject(FacesContext context, UIComponent component, String charsetName) {
+		return Charset.availableCharsets().get(charsetName);
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Charset charset) {
+		return charset.name();
+	}
+
+}
